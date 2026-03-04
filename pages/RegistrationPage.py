e6b91e5f1dd0e97b6c97d978922393bb391cac35
lokeshjammi <jammilokesh3@gmail.com>
2026-03-04 22:21:54 +0530

    Changes commit

--- pages/RegistrationPage.py
+++ pages/RegistrationPage.py
@@ -13,4 +13,11 @@ class RegistrationPage(BasePage):
 
     def read_page_title(self,expected_page_title):
         actual_page_title = self.page.title()
-        assert actual_page_title == expected_page_title        
+        assert actual_page_title == expected_page_title
+
+    def check_registration_form_title(self, registration_form_title):
+        actual_form_title = self.page.locator("//h3[contains(text(), 'Dummy Registration Form')]").inner_text()
+        if actual_form_title:
+            pass
+        else:
+            print("Form is not displayed")
